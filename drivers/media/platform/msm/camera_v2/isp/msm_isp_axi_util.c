cc98ba445ce55783dddb69b6a34c53a6817dcba7
Jim Rasche <jrasche@codeaurora.org>
2013-07-22 15:03:50 -0700

    msm:camera: Fix multiple bounds check
    Added bounds check to user input num_streams at several location,
    without checking a position outside array could be dereferenced
    
    Change-Id: I6e82d8b51e4ec6772316c7daef243240c029db96
    Signed-off-by: Jim Rasche <jrasche@codeaurora.org>
    CVE-2014-9867
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -1082,6 +1082,10 @@ static int msm_isp_start_axi_stream(struct vfe_device *vfe_dev,
 		return -EINVAL;
 	}
 
+	if (stream_cfg_cmd->num_streams > MAX_NUM_STREAM) {
+		return -EINVAL;
+	}
+
 	for (i = 0; i < stream_cfg_cmd->num_streams; i++) {
 		if (HANDLE_TO_IDX(stream_cfg_cmd->stream_handle[i]) >= MAX_NUM_STREAM) {
 			return -EINVAL;
@@ -1144,6 +1148,10 @@ static int msm_isp_stop_axi_stream(struct vfe_device *vfe_dev,
 	int i, rc = 0;
 	struct msm_vfe_axi_stream *stream_info;
 	struct msm_vfe_axi_shared_data *axi_data = &vfe_dev->axi_data;
+
+	if (stream_cfg_cmd->num_streams > MAX_NUM_STREAM) {
+		return -EINVAL;
+	}
 
 	if (stream_cfg_cmd->num_streams > MAX_NUM_STREAM) {
 		return -EINVAL;
