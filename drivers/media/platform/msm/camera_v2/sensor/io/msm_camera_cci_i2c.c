15fd3d334d02c398c04e389428b4f1bcc08a6f79
VijayaKumar T M <vtmuni@codeaurora.org>
2016-09-07 12:53:43 +0530

    msm: camera: Restructure data handling to be more robust
    Use dynamic array allocation instead of static array to
    prevent stack overflow.
    User-supplied number of bytes may result in integer overflow.
    To fix this we check that the num_byte isn't above 8K size.
    
    CRs-Fixed: 1060554
    Change-Id: I9b05b846e5cc3a62b1a0a67be529f09abc764796
    Signed-off-by: VijayaKumar T M <vtmuni@codeaurora.org>
    [haggertk]: Partial, most of original commit was already incorporated
    by Samsung
    CVE-2016-6741
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_cci_i2c.c
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_cci_i2c.c
@@ -49,7 +49,7 @@ int32_t msm_camera_cci_i2c_read(struct msm_camera_i2c_client *client,
 	enum msm_camera_i2c_data_type data_type)
 {
 	int32_t rc = -EFAULT;
-	unsigned char buf[MAX_I2C_ADDR_TYPE_SIZE + MAX_I2C_DATA_TYPE_SIZE];
+	unsigned char *buf = NULL;
 	struct msm_camera_cci_ctrl cci_ctrl;
 
 	if ((client->addr_type != MSM_CAMERA_I2C_BYTE_ADDR
@@ -57,6 +57,17 @@ int32_t msm_camera_cci_i2c_read(struct msm_camera_i2c_client *client,
 		|| (data_type != MSM_CAMERA_I2C_BYTE_DATA
 		&& data_type != MSM_CAMERA_I2C_WORD_DATA))
 		return rc;
+
+	if (client->addr_type > UINT_MAX - data_type) {
+		pr_err("%s: integer overflow prevented\n", __func__);
+		return rc;
+	}
+
+	buf = kzalloc(client->addr_type+data_type, GFP_KERNEL);
+	if (!buf) {
+		pr_err("%s:%d no memory\n", __func__, __LINE__);
+		return -ENOMEM;
+	}
 
 	cci_ctrl.status = 0;//prevent
 	cci_ctrl.cmd = MSM_CCI_I2C_READ;
@@ -69,6 +80,8 @@ int32_t msm_camera_cci_i2c_read(struct msm_camera_i2c_client *client,
 			core, ioctl, VIDIOC_MSM_CCI_CFG, &cci_ctrl);
 	if (rc < 0) {
 		pr_err("%s: line %d rc = %d\n", __func__, __LINE__, rc);
+		kfree(buf);
+		buf = NULL;
 		return rc;
 	}
 	rc = cci_ctrl.status;
@@ -78,6 +91,8 @@ int32_t msm_camera_cci_i2c_read(struct msm_camera_i2c_client *client,
 		*data = buf[0] << 8 | buf[1];
 
 	S_I2C_DBG("%s addr = 0x%x data: 0x%x\n", __func__, addr, *data);
+	kfree(buf);
+	buf = NULL;
 	return rc;
 }
 
