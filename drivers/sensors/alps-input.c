4e9a31c7ecef41de7b5930fc28de65fe71528441
Francescodario Cuzzocrea <bosconovic@gmail.com>
2019-08-06 18:15:33 +0200

    drivers: sensors: alps: switch to alarmtimer interface

--- drivers/sensors/alps-input.c
+++ drivers/sensors/alps-input.c
@@ -205,7 +205,8 @@ static void hscd_poll(struct alps_data *data)
 	int xyz[3];
 	struct input_dev * idev = data->alps_idev->input;
 	int time_hi, time_lo;
-	struct timespec ts = ktime_to_timespec(alarm_get_elapsed_realtime());
+	struct timespec ts;
+    get_monotonic_boottime(&ts);
 	u64 timestamp_new = ts.tv_sec * 1000000000ULL + ts.tv_nsec;
 	u64 delay = data->delay * 1000000ULL;
 	u64 shift_timestamp = delay >> 1;
