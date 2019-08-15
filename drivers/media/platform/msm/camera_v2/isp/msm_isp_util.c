c94ec77c40f38e170f9e851afd127220cfb7b9ec
xXPR0T0TYPEXx <thekurdo97@gmail.com>
2019-08-15 19:34:00 +0200

    camera: backward compatibility for old s3ve3g(ds) camera mm modules
    Allow the camera driver to load old libmmcamera2* modules without the support of flicker_type and new frame_id.

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_util.c
@@ -703,17 +703,21 @@ int msm_isp_proc_cmd(struct vfe_device *vfe_dev, void *arg)
 		goto copy_cmd_failed;
 	}
 
+#if !defined(CONFIG_MACH_S3VE3G_EUR)
 	if( (vfe_dev->frame_id == proc_cmd->frame_id && vfe_dev->eof_event_occur != 1)
 		|| proc_cmd->frame_id == 0) {
+#endif
 	for (i = 0; i < proc_cmd->num_cfg; i++)
 		msm_isp_send_hw_cmd(vfe_dev, &reg_cfg_cmd[i],
 			cfg_data, proc_cmd->cmd_len);
+#if !defined(CONFIG_MACH_S3VE3G_EUR)
 	}
 	else{
 		rc = MSM_VFE_REG_CFG_FRAME_ID_NOT_MATCH_ERROR;
 		pr_err("%s: not match platform_id=%u, kernel_id=%u, eof_event_occur=%u\n",
 			__func__,proc_cmd->frame_id, vfe_dev->frame_id, vfe_dev->eof_event_occur);
 	}
+#endif
 	if (copy_to_user(proc_cmd->cfg_data,
 			cfg_data, proc_cmd->cmd_len)) {
 		rc = -EFAULT;
